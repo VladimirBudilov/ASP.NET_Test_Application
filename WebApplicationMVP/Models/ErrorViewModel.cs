7cad2660e9f7c0321afc48af3529b8072ada388e
VladimirBudilov <109314834+VladimirBudilov@users.noreply.github.com>
2023-12-26 10:29:58 +0400

    init project

---
+++ WebApplicationMVP/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace WebApplicationMVP.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
